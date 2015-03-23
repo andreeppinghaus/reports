603e6d50be3ae5b8d88ada1f7fc13a782e2ac05b
bgiminiani <giminiani@gmail.com>
2015-03-23 14:35:59 -0300

    Genareted base report json file

--- src/app.rb
+++ src/app.rb
@@ -23,7 +23,29 @@ def get_databases
 end
 
 def get_docs(base)
-    http_get("#{Sinatra::Application.settings.couchdb}/http://cncflora.jbrj.gov.br/couchdb/#{base}/_all_docs?include_docs=true")
+    uri = "#{Sinatra::Application.settings.couchdb}/#{base}/_all_docs?include_docs=true"
+    docs = http_get(uri)
+end
+
+def create_report_base(path,name,extension)
+    file = "#{path}/#{name}.#{extension}"
+    if !File.exists?(file)
+        hash = get_docs(name)
+        File.open(file,"w") do |f|
+            f.write(hash.to_json)
+        end
+    end
+    file
+end
+
+def read_json_to_hash(path,name,extension)
+    file = "#{path}/#{name}.#{extension}"
+    if File.exists?(file)
+        hash = JSON.parse(File.read(file))
+    else
+       #handle exception 
+    end
+    hash
 end
 
 def get_reports_names
--- test/report.rb
+++ test/report.rb
@@ -12,17 +12,41 @@ describe "Generation of reports." do
     #  post "/login", { :user=>'{"name":"Bruno","email":"bruno@cncflora.net"}' }
     #end
 
+    before(:all){
+        @uri = "http://localhost:5984"
+        @file_path_base = "data"
+        @file_name_base = "livro_vermelho_2013"
+        @file_extension_base = "json"
+
+        @all_docs = read_json_to_hash(@file_path_base,@file_name_base,@file_extension_base)
+    }
+
+
     it "Check all databases." do
         all_dbs = get_databases
         history = all_dbs.select{ |d| d.end_with? "_history"}
         others = ["_replicator","_users"]
         db = all_dbs - (history+others)
-
         expect(db.count).to eq( (all_dbs-history-others).count )
+        expect(db).to include("livro_vermelho_2013","pan_bacia_alto_tocantins")
     end
 
+
+    it "Check for the base file the reports to be generated." do
+        report_base = create_report_base(@file_path_base,@file_name_base,@file_extension_base)
+        expect(File.exists?(report_base)).to be(true)
+        #File.delete(report_base) if File.exist?(report_base)
+    end
+
+
+    it "Read base report json file to hash." do
+        expect(@all_docs["total_rows"]).to eq(@all_docs["rows"].count)
+    end
+
+    it "Generate assessment report."
+
+
     it "Get index page." do
-        uri = "http://localhost:5984"
         all_dbs = get_databases
         history = all_dbs.select{ |d| d.end_with? "_history"}
         others = ["_replicator","_users"]
@@ -31,12 +55,11 @@ describe "Generation of reports." do
         get "/"
         expect(last_response.body).to have_tag('th',:text=>'RECORTES')
         db.each{ |d|
-            expect(last_response.body).to have_tag('tbody tr td a',:href=>"#{uri}/reports/#{d}",:text=>d.upcase)
+            expect(last_response.body).to have_tag('tbody tr td a',:href=>"#{@uri}/reports/#{d}",:text=>d.upcase)
         }
     end
 
     it "Get the reports of a checklist" do
-        uri = "http://localhost:5984"
         all_dbs = get_databases
         history = all_dbs.select{ |d| d.end_with? "_history"}
         others = ["_replicator","_users"]
@@ -46,7 +69,7 @@ describe "Generation of reports." do
         get "/reports/#{db[0]}"
         expect(last_response.body).to have_tag('div h2',:text=>"Relatórios: #{db[0]}")
         reports.each{ |report|
-            expect(last_response.body).to have_tag('ul li a',:href=>"#{uri}/#{db[0]}/#{report[:name]}",:text=>"#{report[:label]}")
+            expect(last_response.body).to have_tag('ul li a',:href=>"#{@uri}/#{db[0]}/#{report[:name]}",:text=>"#{report[:label]}")
         }
     end
 end
