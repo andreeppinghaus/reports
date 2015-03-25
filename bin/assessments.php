6b60f698b415dcafb80a6b35b585b0f900529bf8
Diogo Souza <diogo@diogok.net>
2015-03-25 10:36:57 -0300

    include rationale on assessment

--- bin/assessments.php
+++ bin/assessments.php
@@ -2,7 +2,7 @@
 
 include 'base.php';
 
-$fields = ["family","scientificName","analysis","assessment","category","criteria"];
+$fields = ["family","scientificNameWithoutAuthorship","scientificNameAuthorship","analysis","assessment","category","criteria","rationale"];
 fputcsv($csv,$fields);
 
 $taxons = [];
@@ -13,6 +13,7 @@ foreach($all->rows as $row) {
             $taxons[$doc->scientificNameWithoutAuthorship] =
                 arraY("family"=>$doc->family,
                     "name"=>$doc->scientificNameWithoutAuthorship,
+                    "author"=>$doc->scientificNameAuthorship,
                     "analysis"=>"",
                     "assessment"=>"",
                     "category"=>"",
@@ -28,6 +29,9 @@ foreach($all->rows as $row) {
         $taxons[$doc->taxon->scientificNameWithoutAuthorship]['analysis'] = $doc->metadata->status;
     } else if($doc->metadata->type=='assessment') {
         $taxons[$doc->taxon->scientificNameWithoutAuthorship]['assessment'] = $doc->metadata->status;
+        if(isset($doc->rationale)) {
+            $taxons[$doc->taxon->scientificNameWithoutAuthorship]['rationale'] = str_replace('?','', strip_tags($doc->rationale) );
+        }
         if(isset($doc->category)) {
             $taxons[$doc->taxon->scientificNameWithoutAuthorship]['category'] = $doc->category;
         }
@@ -39,7 +43,7 @@ foreach($all->rows as $row) {
 
 foreach($taxons as $taxon) {
   $data=[
-    $taxon["family"],$taxon["name"],$taxon["analysis"],$taxon["assessment"],$taxon["category"],$taxon["criteria"]
+    $taxon["family"],$taxon["name"],$taxon["author"],$taxon["analysis"],$taxon["assessment"],$taxon["category"],$taxon["criteria"],$taxon["rationale"]
   ];
   fputcsv($csv,$data);
 }
