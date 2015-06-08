1931910c54c6bbc7d6ec80242cdefd60d0396791
Diogo Souza <diogo@diogok.net>
2015-06-08 15:34:03 -0300

    fix csv2xlsx

--- bin/csv2xlsx.py
+++ bin/csv2xlsx.py
@@ -2,19 +2,20 @@ import xlwt, csv, os
 
 for fol in os.listdir("./data/"):
     csv_folder = "./data/"+fol+"/"
-    print fol
+    if os.path.isdir(csv_folder):
+        print fol
 
-    book = xlwt.Workbook(encoding="UTF-8")
-    for fil in os.listdir(csv_folder):
-        if fil.endswith(".csv") and fil != "occurrences.csv":
-            print fil
-            sheet = book.add_sheet(fil[:-4])
-            with open(csv_folder + fil) as filname:
-                reader = csv.reader(filname)
-                i = 0
-                for row in reader:
-                    for j, each in enumerate(row):
-                        sheet.write(i, j, each)
-                    i += 1
+        book = xlwt.Workbook(encoding="UTF-8")
+        for fil in os.listdir(csv_folder):
+            if fil.endswith(".csv") and fil != "occurrences.csv":
+                print fil
+                sheet = book.add_sheet(fil[:-4])
+                with open(csv_folder + fil) as filname:
+                    reader = csv.reader(filname)
+                    i = 0
+                    for row in reader:
+                        for j, each in enumerate(row):
+                            sheet.write(i, j, each)
+                        i += 1
 
-    book.save("./data/"+fol+"/all.xls")
+        book.save("./data/"+fol+"/all.xls")
