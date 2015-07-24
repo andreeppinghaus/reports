b28036a23eb4a72ac83ced91a07b6d1d4cc80c89
Diogo Souza <diogo@diogok.net>
2015-07-24 15:45:32 -0300

    simple list

--- data/index.php
+++ data/index.php
@@ -39,7 +39,7 @@ if($tmp === false)
 if(substr($tmp, 0,strlen(__DIR__)) !== __DIR__)
 	err(403,"Forbidden");
 
-if(!$_COOKIE['_sfm_xsrf'])
+if(!isset($_COOKIE['_sfm_xsrf']))
 	setcookie('_sfm_xsrf',bin2hex(openssl_random_pseudo_bytes(16)));
 if($_POST) {
 	if($_COOKIE['_sfm_xsrf'] !== $_POST['xsrf'] || !$_POST['xsrf'])
@@ -63,8 +63,11 @@ if($_GET['do'] == 'list') {
 	        	'name' => basename($i),
 	        	'path' => preg_replace('@^\./@', '', $i),
 	        	'is_dir' => is_dir($i),
+            'is_deletable'=>false,
+            /*
 	        	'is_deleteable' => (!is_dir($i) && is_writable($directory)) || 
 	        					   (is_dir($i) && is_writable($directory) && is_recursively_deleteable($i)),
+             */
 	        	'is_readable' => is_readable($i),
 	        	'is_writable' => is_writable($i),
 	        	'is_executable' => is_executable($i),
@@ -76,17 +79,17 @@ if($_GET['do'] == 'list') {
 	echo json_encode(array('success' => true, 'is_writable' => is_writable($file), 'results' =>$result));
 	exit;
 } elseif ($_POST['do'] == 'delete') {
-	rmrf($file);
+	#rmrf($file);
 	exit;
 } elseif ($_POST['do'] == 'mkdir') {
-	chdir($file);
-	@mkdir($_POST['name']);
+	#chdir($file);
+	#@mkdir($_POST['name']);
 	exit;
 } elseif ($_POST['do'] == 'upload') {
-	var_dump($_POST);
-	var_dump($_FILES);
-	var_dump($_FILES['file_data']['tmp_name']);
-	var_dump(move_uploaded_file($_FILES['file_data']['tmp_name'], $file.'/'.$_FILES['file_data']['name']));
+	#var_dump($_POST);
+	#var_dump($_FILES);
+	#var_dump($_FILES['file_data']['tmp_name']);
+	#var_dump(move_uploaded_file($_FILES['file_data']['tmp_name'], $file.'/'.$_FILES['file_data']['name']));
 	exit;
 } elseif ($_GET['do'] == 'download') {
 	$filename = basename($file);
@@ -391,6 +394,7 @@ $(function(){
 </script>
 </head><body>
 <div id="top">
+<!--
 	<form action="?" method="post" id="mkdir" />
 		<label for=dirname>Create New Folder</label><input id=dirname type=text name=name value="" />
 		<input type="submit" value="create" />
@@ -400,6 +404,7 @@ $(function(){
 		<b>or</b>
 		<input type="file" multiple />
 	</div>
+-->
 	<div id="breadcrumb">&nbsp;</div>
 </div>
 
