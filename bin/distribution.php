70e0a6c8f8ba63bbe2f5ac09972f5ec8764b74c0
Diogo Souza <diogo@diogok.net>
2015-08-06 16:06:04 -0300

    back with distribution data, but reduced

--- bin/distribution.php
+++ bin/distribution.php
@@ -2,48 +2,66 @@
 
 include 'base.php';
 
-return;
-echo "\"Familia\",\"Especie\",\"Endemica\",\"Altitude\",\"EOO\",\"AOO\",\"Válidos\",\"Inválidos\",\"Não validados\",\"Total registros\"\n";
+#$fields = ['family','scientificName','endemic','altitude','eoo','aoo'];
+$fields = ['family','scientificName','endemic','altitude'];
+fputcsv($csv,$fields);
+
+$taxons = [];
+$names  = [];
+foreach($all->rows as $row) {
+    $doc = $row->doc;
+    if($doc->metadata->type=='taxon') {
+      if(isset($doc->scientificNameWithoutAuthorship) && strlen($doc->scientificNameWithoutAuthorship) > 1) {
+        $taxons[] = $doc;
+        if($doc->taxonomicStatus == 'accepted') {
+          $names[$doc->scientificNameWithoutAuthorship]=[$doc->scientificNameWithoutAuthorship];
+        }
+      }
+    }
+}
+
+foreach($taxons as $taxon) {
+  if($taxon->taxonomicStatus == 'synonym') {
+    foreach($taxons as $taxon2) {
+      if($taxon2->acceptedNameUsage==$taxon->acceptedNameUsage || $taxon2->scientificNameWithoutAuthorship==$taxon->acceptedNameUsage) {
+        $names[$taxon2->scientificNameWithoutAuthorship][] = $taxon->scientificNameWithoutAuthorship;
+      }
+    }
+  }
+}
 
 foreach($all->rows as $row) {
+  if(!is_object($row) || !isset($row->doc)) continue;
   $d = $row->doc;
   if($d->metadata->type=='profile') {
-    echo $d->taxon->family.";".$d->taxon->scientificNameWithoutAuthorship.";";
+    $data = [$d->taxon->family,$d->taxon->scientificNameWithoutAuthorship,"",""];
 
     if(isset($d->distribution) && is_object($d->distribution)) {
-      echo $d->distribution->brasilianEndemic.";";
+      $data[2] =$d->distribution->brasilianEndemic;
 
       if(isset($d->distribution->altitude)) {
         $a = $d->distribution->altitude;
         if(isset($a->absolute)) {
-          echo $a->absolute.";";
+          $data[3] = $a->absolute;
         } else if(isset($a->minimum) && isset($a->maximum)) {
-          echo $a->minimum."~".$a->maximum.";";
+          $data[3] = $a->minimum."~".$a->maximum;
         } else {
-          echo $a->minimum.$a->maximum.";";
+          $data[3] = $a->minimum.$a->maximum;
         }
-      } else {
-        echo ";";
       }
-    } else {
-      echo ";;";
     }
 
-    /* got to use dwc */
     /*
     $url = 'http://cncflora.jbrj.gov.br/occurrences/'.$base.'/specie/'.urlencode($d->taxon->scientificNameWithoutAuthorship).'?json=true';
-    shell_exec("curl '".$url."' -L -H 'Cookie: rack.session=485de10e24d76f25233101173b83b22347c8ac40e06e6cb1824c55e5cb8a9d06' > tmp_occs.json");
-    $data = file_get_contents("tmp_occs.json");
-    $json = json_decode($data);
-    */
+    $sess='1b71063e4887a009fd6742376c6f92a52036817eb443ac8479750dac81233d42';
+    shell_exec("curl '".$url."' -L -H 'Cookie: rack.session=".$sess."' > tmp_occs.json");
+    $resp = file_get_contents('tmp_occs.json');
+    $json = json_decode($resp);
 
-    echo $json->stats->eoo.";";
-    echo $json->stats->aoo.";";
-    echo $json->stats->valid.";";
-    echo $json->stats->invalid.";";
-    echo $json->stats->not_validated.";";
-    echo $json->stats->total.";";
-    echo "\n";
+    $data[4] = $json->stats->eoo;
+    $data[5] = $json->stats->aoo;
+     */
+    fputcsv($csv,$data);
   }
 }
 
