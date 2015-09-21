bdb540696cdfb36aefdc30b1189da6d0b206e2bd
Laura Moraes <lmoraes@cncflora.net>
2015-09-21 15:40:20 -0300

    Adapted actions.php

--- bin/actions.php
+++ bin/actions.php
@@ -1,6 +1,7 @@
 <?php
 
 include 'base.php';
+include 'utils/gdrive.php';
 
 $fields = ["family","scientificName","action","situation",'details','references'];
 fputcsv($csv,$fields);
@@ -10,26 +11,35 @@ foreach($all->rows as $row) {
   if($d->metadata->type=='profile') {
     if(isset($d->actions) && is_array($d->actions)) {
       foreach($d->actions as $t) {
-        if(isset($t->action)) {
-            print_r($t->action);
-          $data=[
-            $d->taxon->family
-            ,$d->taxon->scientificNameWithoutAuthorship
-            ,$t->action
-            ,$t->situation
-            ,$t->details
-            ,implode(";",$t->references)
-          ];
-            //All keys have to be the header name but lowercase
-          $row_gdrive = array(
-              "family" => $d->taxon->family,
-              "scientificname" => $d->taxon->scientificNameWithoutAuthorship,
-              "action" => $t->action,
-              "situation" => $t->details,
-              "references" => implode(";", $t->references));
-            fputcsv($csv,$data);
-            $data_gdrive[] = $row_gdrive;
-        }
+          if(isset($t->action)) {
+              if (!array_key_exists("situation", $t)){
+                  $t->situation = "";
+              }
+              if (!array_key_exists("details", $t)){
+                  $t->details = "";
+              }
+              if (!array_key_exists("references", $t)){
+                  $t->references = array();
+              }
+              $data=[
+                $d->taxon->family
+                ,$d->taxon->scientificNameWithoutAuthorship
+                ,$t->action
+                ,$t->situation
+                ,$t->details
+                ,implode(";",$t->references)
+              ];
+                //All keys have to be the header name but lowercase
+              $row_gdrive = array(
+                  "family" => $d->taxon->family,
+                  "scientificname" => $d->taxon->scientificNameWithoutAuthorship,
+                  "action" => $t->action,
+                  "situation" => $t->situation,
+                  "details" => $t->details,
+                  "references" => implode(";", $t->references));
+                fputcsv($csv,$data);
+              $data_gdrive[] = $row_gdrive;
+          }
       }
     }
   }
