6872d7cb77e5d29702166a03351639e39b5235db
Ícaro de Carvalho <decarvalho.icaro@gmail.com>
2017-01-31 15:11:34 -0200

    Adicionando cabeçalho EOO e AOO

--- src/cncflora/reports/Assessments.php
+++ src/cncflora/reports/Assessments.php
@@ -7,7 +7,7 @@ class Assessments{
   public $title = "Avaliações";
   public $description = "Lista com as avaliações de risco de extensão de cada espécie.";
   public $is_private = false;
-  public $fields = ["familia","nome científico","autor","status no workflow","categoria","critério", "avaliador", "revisor", "justificativa", "data da avaliacao", "bioma"];
+  public $fields = ["familia","nome científico","autor","status no workflow","categoria","critério", "avaliador", "revisor", "justificativa", "data da avaliacao", "bioma", "eoo(km²)", "aoo(km²)"];
   public $filters=["checklist",'family'];
 
   function run($csv,$checklist,$family="") {
